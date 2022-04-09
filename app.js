8d5b91dc75f1bae67158360f0a179f3cd52e75cc
jesse-crypted <jesseolatunjioluwadare@gmail.com>
2022-04-09 11:11:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
-todoButton.addEventListener("click", addTodo);
+todoButton.addEventListener("submit", addTodo);
 todoList.addEventListener("click", deleteCheck);
 
 //Functions
